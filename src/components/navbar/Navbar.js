af3d59a83194c0c443c8026b23fc14e833e563c9
Ajay Singh <contact@ajaysingh.ca>
2023-06-19 22:10:00 -0400

    add basic nav bar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Navbar.scss'
+export default function Navbar() {
+  return (
+   <nav className='nav-item'>
+        <div className='nav-item__logo'>OrbitView</div>
+   </nav>
+  )
+}
